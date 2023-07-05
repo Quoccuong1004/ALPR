f49974a4568f1a0a7852bac9d40947e2dc5130cd
Quoc Cuong <89184885+Quoccuong1004@users.noreply.github.com>
2023-07-05 19:06:55 +0700

    update model.py

---
+++ model.py
@@ -0,0 +1,34 @@
+import torchvision
+
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.ssd import SSDClassificationHead
+from torchvision.models.detection import _utils
+
+def create_model(num_classes):
+    
+    # load Faster RCNN pre-trained model
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    
+    # get the number of input features 
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # define a new head for the detector with required number of classes
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes) 
+
+    return model
+
+def create_model_ssd(num_classes,size):
+
+    #load ssd pre-train model
+    model = torchvision.models.detection.ssd300_vgg16(weights=torchvision.models.detection.SSD300_VGG16_Weights.DEFAULT)
+
+    in_channels = _utils.retrieve_out_channels(model.backbone, (size, size))
+    # List containing number of anchors based on aspect ratios.
+    num_anchors = model.anchor_generator.num_anchors_per_location()
+    # The classification head.
+    model.head.classification_head = SSDClassificationHead(
+        in_channels=in_channels,
+        num_anchors=num_anchors,
+        num_classes=num_classes,
+    )
+    return model
+    
